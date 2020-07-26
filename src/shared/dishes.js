65df5c6e36278fc0cd4bcec13ee934e928416238
mehdi.sep@gmail.com <mehdi.sep@gmail.com>
2020-07-26 22:02:24 +1000

    Components Part2

--- src/shared/dishes.js
+++ src/shared/dishes.js
@@ -3,7 +3,7 @@ export const DISHES =
         {
         id: 0,
         name:'Uthappizza',
-        image: 'assets/uthappizza.png',
+        image: 'assets/images/uthappizza.png',
         category: 'mains',
         label:'Hot',
         price:'4.99',
@@ -48,7 +48,7 @@ export const DISHES =
         {
         id: 1,
         name:'Zucchipakoda',
-        image: 'assets/zucchipakoda.png',
+        image: 'assets/images/zucchipakoda.png',
         category: 'appetizer',
         label:'',
         price:'1.99',
@@ -94,7 +94,7 @@ export const DISHES =
         {
         id: 2,
         name:'Vadonut',
-        image: 'assets/vadonut.png',
+        image: 'assets/images/vadonut.png',
         category: 'appetizer',
         label:'New',
         price:'1.99',
@@ -140,7 +140,7 @@ export const DISHES =
         {
         id: 3,
         name:'ElaiCheese Cake',
-        image: 'assets/elaicheesecake.png',
+        image: 'assets/images/elaicheesecake.png',
         category: 'dessert',
         label:'',
         price:'2.99',
