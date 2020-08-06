bfc51f3e5ea62e29e8244cec15a16ae6d8ebd639
mehdi.sep@gmail.com <mehdi.sep@gmail.com>
2020-08-06 14:36:03 +1000

    Assignment 3

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,7 +1,118 @@
-import React from 'react';
-import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
+import React, { Component } from 'react';
+import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle, Input, Button, 
+    Col, Row, Modal, ModalHeader, ModalBody, Form, FormGroup, Label} from 'reactstrap';
 import {Breadcrumb, BreadcrumbItem} from 'reactstrap';
+import {Control, LocalForm, Errors} from 'react-redux-form';
 import {Link} from 'react-router-dom';
+
+const required = (val) => val && val. length;
+const maxLength = (len) => (val) => !(val) || (val.length <= len);
+const minLength = (len) => (val) => (val) && (val.length >= len);
+
+class CommentForm extends Component{
+    constructor(props){
+        super(props);
+        this.state={
+            isModalOpen: false
+        };
+        this.toggleModal = this.toggleModal.bind(this);
+        this.handleSubmitComment = this.handleSubmitComment.bind(this);
+    }
+
+    toggleModal(){
+        this.setState({
+            isModalOpen: !this.state.isModalOpen
+        });
+    }
+
+    handleSubmitComment(event){
+        this.toggleModal();
+            alert("Your Name " + this.yourname.value);
+        event.preventDefault();
+    }
+
+    handleSubmitComment(values){
+        console.log('Current State is: ' + JSON.stringify(values));
+        alert('Current State is: ' + JSON.stringify(values));
+    }
+
+    validate(yourname){
+        const errors={
+            yourname:'',
+        };
+        if (this.state.touched.yourname && yourname.length < 3)
+        errors.yourname = 'First Name should be >= 3 characters';
+        else if (this.state.touched.yourname && yourname.length > 15)
+        errors.yourname = 'First Name should be <= 15 characters';
+
+        return errors;
+    }
+    
+        render(){
+            return(
+                <div className="container">
+                <LocalForm onSubmit={(values) => this.handleSubmitComment(values)}>
+                <Button  outline color="secondary" onClick={this.toggleModal}>
+                <span className="fa fa-pencil fa-lg"></span>Submit Comment</Button>
+
+                <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+                <ModalHeader toggle={this.toggleModal}>Submit Comment</ModalHeader>
+                <ModalBody>
+                    <LocalForm onSubmit={(values) => this.handleSubmitComment(values)}>
+                            <FormGroup>
+                                <Label htmlFor="rating">Rating</Label>
+                                <Row className="form-group">
+                                <Col md={12}>
+                                <Control.select model=".rating" name="rating" className="form-control">
+                                    <option>1</option>
+                                    <option>2</option>
+                                    <option>3</option>
+                                    </Control.select>
+                                    </Col>
+                                    </Row>
+                            </FormGroup>
+                            
+                            <FormGroup>
+                            <Label htmlFor="yourname" >Your Name</Label>
+                            <Row className="form-group">
+                            <Col md={12}>
+                                    <Control.text model=".yourname" id="yourname" name="yourname"
+                                        placeholder="Your Name" className="form-control" 
+                                        validators={{
+                                            required, minLength: minLength(3), maxLength: maxLength(15)
+                                        }}
+                                         />
+                                         <Errors className="text-danger"
+                                         model=".yourname"
+                                         show="touched"
+                                         messages={{
+                                             required: 'Required',
+                                             minLength: 'Must be greater than 2 characters',
+                                             maxLength: 'Must be 15 characters or less'
+                                         }}/>
+                                </Col>
+                                </Row>
+                        </FormGroup>
+
+                        <FormGroup>
+                        <Label htmlFor="message">Comment</Label>
+                            <Row className="form-group">
+                                <Col md={12}>
+                                    <Control.textarea model=".message" id="message" name="message"
+                                    rows="6" className="form-control"/>
+                                </Col>
+                                </Row>
+                            </FormGroup>
+                        <Button type="submit" value="submit" color="primary" className="primary">Submit</Button>
+                    </LocalForm>
+                </ModalBody>
+                </Modal>
+                </LocalForm>
+                </div>
+            );
+        
+    }
+}
 
    //user defined components always start with capital letters
     function RenderDish ({dish}) {
@@ -12,7 +123,7 @@ import {Link} from 'react-router-dom';
                     <CardBody>
                         <CardTitle>{dish.name}</CardTitle>
                         <CardText>{dish.description}</CardText>
-                    </CardBody>
+                        </CardBody>
                 </Card>
             );
         else
@@ -20,6 +131,7 @@ import {Link} from 'react-router-dom';
                 <div></div>
             );
     }
+
     function RenderComments({comments}) {
         if (comments.length != 0) {
             return (
@@ -30,10 +142,12 @@ import {Link} from 'react-router-dom';
                             <li>
                                 <p>{comment.comment}</p>
                                 <p>-- {comment.author} , {new Intl.DateTimeFormat('en-AU',{year:'numeric', month:'short', day:'2-digit'}).format(new Date(Date.parse(comment.date)))}</p>
-                            </li>
+                                
+                             </li>                               
                         </ul>
                     )
                     )}
+                    <CommentForm/>
                 </div>
             );
         }
@@ -41,7 +155,6 @@ import {Link} from 'react-router-dom';
             return (
                 <div></div>
             );
-
         }
     }
    
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -38,7 +38,7 @@ class Main extends Component {
 
     const DishWithId = ({match}) =>{
         return (
-            <DishDetail dish={this.spropstate.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10)) [0]}
+            <DishDetail dish={this.props.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10)) [0]}
             comments={this.props.comments.filter((comment) => comment.dishId === parseInt (match.params.dishId,10))}
             />
         );
